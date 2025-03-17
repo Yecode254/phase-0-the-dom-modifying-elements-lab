922cf499ed30ce5180176854ebb03d75839f7994
Yego <eddieyego273@gmail.com>
2025-03-17 13:11:22 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+// // Write your code here!
+document.querySelector('main').remove();
+ const newHeader = document.createElement("h1");
+ newHeader.setAttribute('id',"victory");
+ newHeader.innerHTML = "Eddy is the champion";
